9141d8c41a0b7a2142884db27c59b6e94f9057ac
JayDee <github_cloudlabsuser_1257@cloudlabsaiuser.com>
2025-06-12 15:21:27 +0000

    create routing

--- src/index.js
+++ src/index.js
@@ -2,12 +2,42 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import AboutUs from './components/AboutUs';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
+const Navigation = () => (
+  <nav className="navbar navbar-expand-lg navbar-light bg-light mb-4">
+    <div className="container">
+      <Link className="navbar-brand" to="/">DevSecOps</Link>
+      <div className="collapse navbar-collapse">
+        <ul className="navbar-nav ms-auto">
+          <li className="nav-item">
+            <Link className="nav-link" to="/">Home</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/about">About Us</Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+);
+
+const Root = () => (
+  <Router>
+    <Navigation />
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/about" element={<AboutUs />} />
+    </Routes>
+  </Router>
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>
 );
 
